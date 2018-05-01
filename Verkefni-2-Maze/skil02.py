b0c52b5d3f1c8cc0045d698a42aa786d9c8b34a0
AtliOskarsson <atlio@outlook.com>
2018-05-01 17:28:32 +0000

    Klárað 👍

--- Verkefni-2-Maze/skil02.py
+++ Verkefni-2-Maze/skil02.py
@@ -4,15 +4,17 @@
 import os
 import random
 import pygame
+pygame.font.init()
 
 
+font = pygame.font.SysFont("Calibri", 15)
 # Class for the orange dude
 class Player(object):
 
-    def __init__(self):
+    def __init__(self, antibombcount = 0, defusedBombs = 0):
         self.rect = pygame.Rect(16, 16, 16, 16)
-        self.antibombcount = 0
-        self.defusedBombs = 0
+        self.defusedBombs = defusedBombs
+        self.antibombcount = antibombcount
 
     def move(self, dx, dy):
 
@@ -24,13 +26,6 @@ class Player(object):
 
     def anti(self):
         return self.antibombcount
-
-    def draw(self):
-        if self.antibombcount >= 1:
-            man = (255, 200, 0)
-        else:
-            man = (255, 128, 0)
-        return man
 
     def move_single_axis(self, dx, dy):
 
@@ -159,54 +154,54 @@ count = 0
 # Holds the level layout in a list of strings.
 level = [
     "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
-    "W WA      W                                   WW           W",
-    "W WWW W W WWWWWWWWWWW WWWWWWWWWWWWWWWWWWWWWWW WW WWWWWWWWW W",
-    "WA    W W           W W                          W         W",
-    "WBWWWWW WWWWWW WWWWWW W WWWWWWWWWWWWWWWWWWWWWWWWWW WWWWW WWW",
+    "W WA      W     AAAAA                         WW           W",
+    "W WWWBWBWBWWWWWWWWWWW WWWWWWWWWWWWWWWWWWWWWWWBWWBWWWWWWWWWBW",
+    "WA    W W         AAW W                          W         W",
+    "WBWWWWW WWWWWWBWWWWWWBWBWWWWWWWWWWWWWWWWWWWWWWWWWW WWWWWBWWW",
     "W W       W           W W                        W   W   W W",
-    "W WWWWWWWWW WWWWWW WWWW W WWWWWWWWWWWWWWWWWWWWWWWWWWWW W W W",
-    "W W       W W    W W  W   W        W        W   W   W  W   W",
+    "W WWWWWWWWWBWWWWWWBWWWW W WWWWWWWWWWWWWWWWWWWWWWWWWWWW W W W",
+    "W W      AW W    W W  W   W       AW        W   W   W  W   W",
     "W W WWWWWWW W WW W W WWWWWW WWWWWWWW WWWWWW W W W W W WWWW W",
     "W W         W WW W W W      W        W        W   W W    W W",
-    "W WWW WWWWWWW W  W W W WWW  W WWWW WWW WWWWWWWWWWWW WWWW W W",
+    "W WWW WWWWWWW W  W W W WWW  W WWWW WWW WWWWWWWWWWWW WWWWBW W",
     "W W   W       WWWW W W W WWWW WW   W    W W   W   W    W W W",
     "W W WWW WWWWWWW    W W W       W WWW WWWW W W W W W WWWW W W",
-    "W W W   W       WWWW W W WWWWW W W   W      W W W   W    W W",
-    "W W W WWW WWWWWWW      W     W W WWWWWWWWWWWW W WWWWW WWWW W",
-    "W W W  W  W       WWWWWWWWWW W W              W     W    W W",
-    "W W WW W WW WWWWWWW          W W WWWWWWWWWWWWWWWWWWWWWWW WWW",
-    "W        WW W     W WWWWWWWWWW W W   W   W   W   W   W   W W",
-    "W WWWWW WW  W WWWWW W          W   W   W   W   W   W   W   W",
-    "W W     WW WW W   W W WWWW WWWWWWWWWWWWWWW WWWWWWWWWWWWWWWWW",
-    "W W WWWWW  W  W W W W W  W W             W W               W",
-    "W W W     WW WW WWWWW WW W WWWWWWWWWWWWW W W WWWWWWWWWWWW  W",
-    "W   W WWWWW  W           W               W   W          W  W",
-    "W W W WW     W WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW W  W",
-    "W W      WWW W                              W         W W  W",
-    "W WWWWWWWW W WWWWWWWW                       W WWWWWWWWW W  W",
-    "W W          W                              W W       W W  W",
-    "W     WWWWWWWW                              W W WWWWW W W  W",
-    "W W                                           W W W W W W  W",
-    "W   W                                       WWW W W W      W",
-    "W W W                               B       WWWWWWWWWWWWWWWW",
-    "W W W                                                      W",
-    "W W W                                                      W",
-    "W W W              X                                       W",
-    "W W W                                                      W",
-    "W W W                                                      W",
-    "W W WWWWW               B                                  W",
-    "W W     W                                                  W",
-    "W WWWWW W                                                  W",
-    "W W   W W                                                  W",
-    "W W W W W                                                  W",
-    "W W W W W                                                  W",
-    "W WWW W WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWXW",
-    "W     W                                                  X E",
+    "W W W   W       WWWW W W WWWWW W W   WAA    W W W   W    W W",
+    "W W W WWW WWWWWWW    B W     W W WWWWWWWWWWWW W WWWWW WWWW W",
+    "W W W  W  W       WWWWWWWWWW W W              W   AAW    W W",
+    "W W WW WBWW WWWWWWW          W W WWWWWWWWWWWWWWWWWWWWWWW WWW",
+    "W        WW W     W WWWWWWWWWW W W  BW  BW   W   W   W   W W",
+    "W WWWWWBWW  W WWWWW W          W   W   W   W   W   W   W   W",
+    "W W     WW WW W AAW WBWWWWBWWWWWWWWWWWWWWW WWWWWWWWWWWWWWWWW",
+    "W W WWWWW  W  W W W W W  W WAA           W W               W",
+    "W W W     WW WWBWWWWW WW W WWWWWWWWWWWWW W W WWWWWWWWWWWW BW",
+    "W   W WWWWWA W           W               W   WAA        W  W",
+    "W W WBWW     WBWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW W  W",
+    "W W      WWW W        W         W           W         W WB W",
+    "W WWWWWWWW W WWWWWWWWBW WWWWWWW W WWWWWWWWW W WWWWWWWWW W  W",
+    "W W        B W        W W       W W         W W       W W  W",
+    "W   WWWWWWWWWWBWWWWWWWW WWWWWWWWW W WWWWWWWWW W WWWWW W W BW",
+    "W WWW   W W    W        W         W           W W W W W W  W",
+    "W B W W   W WWWW WWWW WWWWW WWWWWWWWWWWWWWWWWWW W W W      W",
+    "W W W WWW W W    W          W       B         WWWWWWWWWWWWWW",
+    "W W W   W   W WWWWWWWWWWWWWWW WWWWWWWWWWWWWWW              W",
+    "W W WWW WWW W   W           W               WWWWWWWWWWWWWW W",
+    "W W W  AW   WWW W WWWWWWWWWWWWWWWWWWWWWWWWW              W W",
+    "W W W WWWWW   W W W                       WWWWWWWWWWWWWW W W",
+    "W WAW     WWW W W W WWWWWWWWWWWWWWWWWWWWW              W W W",
+    "W WWWWWWW W W W   W W   W              WWWWWWWWWWWWWWWWW W W",
+    "W W     W W W W WWW W W W WWWWWWWWWWWW                   W W",
+    "W WWWWW W W W W W W W WWW W          WWWWWWWWWWWWWWWWWWWWW W",
+    "W W   W W W   W                      W                   W W",
+    "W W W W W WWWWWWWWWWWWWWWWWWWWWWWWWWWW WWWWWW WWWWWWWWWWWW W",
+    "W W W W W               W            W      W W   W   W    W",
+    "W WWW W WWWWWWWWWWWWWWW WWWWWWWWWWWWWWWWWWW W W W W W W W WW",
+    "W     W                                     W   W   W   W  E",
     "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
 
 ]
 
-# Parse the level string above. W = wall, E = exit
+# Parse the level string above. W = wall, B = bomb, A = AntiBomb, E = exit
 x = y = 0
 for row in level:
     for col in row:
@@ -245,15 +240,24 @@ while running:
         player.move(0, -2)
     if key[pygame.K_DOWN]:
         player.move(0, 2)
+    if key[pygame.K_a]:
+        player.move(-1,0)
+    if key[pygame.K_d]:
+        player.move(1, 0)
+    if key[pygame.K_w]:
+        player.move(0, -1)
+    if key[pygame.K_s]:
+        player.move(0, 1)
 
-    # Just added this to make it slightly fun ;)
+
+    # If Player hits the end
     if player.rect.colliderect(endakubbur):
         raise SystemExit("You Win!")
 
     # Draw the scene
     screen.fill((0, 0, 0))
     for wall in walls:
-        pygame.draw.rect(screen, (255, 255, 255), wall.rect)
+        pygame.draw.rect(screen, (209, 209, 209), wall.rect)
     for bomb in bombs:
         pygame.draw.rect(screen, (255,0,255), bomb.rect)
     for antibomb in antibombs:
@@ -265,15 +269,26 @@ while running:
 
     # Takes from antibombcounts in the class Player
     if player.antibombcount >= 1:
-        man = (255, 200, 0)
+        pygame.draw.rect(screen, (186, 188, 47), player.rect)
     else:
-        man = (255, 128, 0)
+        pygame.draw.rect(screen, (255, 200, 0), player.rect)
 
-    pygame.draw.rect(screen, man, player.rect)
-    # counter
+    playersbombs = font.render(str(player.antibombcount), True, (0,0,0))
+    screen.blit(playersbombs, player.rect)
+
+    time = font.render("Seconds: " + str(count), True, (0,0,0))
+    screen.blit(time, (0,0))
+
+    counterOfBombs = font.render("Bombs: " + str(len(bombs)), True, (0,0,0))
+    screen.blit(counterOfBombs, (100, 0))
+
+    counterOfAntiBombs = font.render("Anti-bombs left: " + str(len(antibombs)), True, (0,0,0))
+    screen.blit(counterOfAntiBombs, (200, 0))
+
+
+
     counter += 1
     if counter % 60 == 0:
         count += 1
-        print(count)
 
     pygame.display.flip()
